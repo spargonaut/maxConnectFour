d0356a486ca6d19539d145b9525d8172361f2b1c
jspargo <ThoughtWorks@JS8634.local>
2013-01-08 01:39:44 -0600

    cleaned up BoardReader some more

--- maxConnectFour/gameboard/BoardReader.java
+++ maxConnectFour/gameboard/BoardReader.java
@@ -6,50 +6,30 @@ import java.io.IOException;
 
 import maxConnectFour.GameBoard;
 
-
-
 public class BoardReader {
 
 	private int[][] playBoard;
-	private int pieceCount;
 	
 	public GameBoard readGame(String inputFile)
 		throws IOException {
 		
 		this.playBoard = new int[6][7];
-		this.pieceCount = 0;
-		int counter = 0;
-		
-		String gameData = null;
-
+		String rowData = null;
 		BufferedReader input = new BufferedReader( new FileReader( inputFile ) );
 
-		for(int i = 0; i < 6; i++) {
-			gameData = input.readLine();
-
-			// read each piece from the input file
-			for( int j = 0; j < 7; j++ ) {
-
-				this.playBoard[ i ][ j ] = Integer.parseInt(Character.toString(gameData.charAt(counter++)));
-
-				// sanity check
-				checkIfMarkIsValidOrExit(i, j);
-
-				if( this.playBoard[ i ][ j ] > 0 )
-				{
-					this.pieceCount++;
-				}
+		for(int row = 0; row < 6; row++) {
+			rowData = input.readLine();
+			for( int column = 0; column < 7; column++ ) {
+				markPlayAtPosition(rowData, row, column);
 			}
-
-			//reset the counter
-			counter = 0;
-
 		}
-		
 		input.close();
-
 		return new GameBoard(playBoard);
-		
+	}
+
+	private void markPlayAtPosition(String gameData, int row, int column) {
+		this.playBoard[ row ][ column ] = Integer.parseInt(Character.toString(gameData.charAt(column)));
+		checkIfMarkIsValidOrExit(row, column);
 	}
 
 	private void checkIfMarkIsValidOrExit(int i, int j) {
@@ -64,8 +44,4 @@ public class BoardReader {
 			   ( this.playBoard[ i ][ j ] == 1 ) ||
 			   ( this.playBoard[ i ][ j ] == 2 ) );
 	}
-	
-	public int getCurrentTurn() {
-		return ( this.pieceCount % 2 ) + 1 ;
-	} 
 }
