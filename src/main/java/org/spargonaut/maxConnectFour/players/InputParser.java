63ca926413e99ca18f9b4ad093777a7ec7b49c46
jamesspargo <jspargo@thoughtworks.com>
2015-05-09 17:11:43 -0500

    JS - create the InputParser to get valid numeric input from the user

---
+++ src/main/java/org/spargonaut/maxConnectFour/players/InputParser.java
@@ -0,0 +1,63 @@
+package org.spargonaut.maxConnectFour.players;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputParser {
+
+    BufferedReader bufferedReader;
+
+    public InputParser() {
+        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public InputParser(BufferedReader bufferedReader) {
+        this.bufferedReader = bufferedReader;
+    }
+
+    public int getNextPlayFromHuman() {
+        boolean playIsInvalid = true;
+        int columnToPlay = -1;
+        while(playIsInvalid) {
+            String userInput = null;
+            tellUserToChooseAColumn();
+            userInput = readInput(userInput);
+
+            while( ( userInput.equals("") ) ) {
+                System.out.println("\nI didn't understand that input");
+                tellUserToTryAgain();
+                tellUserToChooseAColumn();
+                userInput = readInput(userInput);
+            }
+
+            Character userChar = userInput.charAt(0);
+
+            if( Character.isDigit( userChar )) {
+                columnToPlay = Character.getNumericValue(userChar);
+                playIsInvalid = false;
+            } else {
+                System.out.println("\nThat was not a valid digit.");
+                tellUserToTryAgain();
+            }
+        }
+        return columnToPlay;
+    }
+
+    private void tellUserToTryAgain() {
+        System.out.println("--Try again");
+    }
+
+    private void tellUserToChooseAColumn() {
+        System.out.print("\nChoose a Column ( 1 - 7 ) -->:");
+    }
+
+    private String readInput(String userInput) {
+        try {
+            userInput = bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return userInput;
+    }
+}
---
+++ src/test/java/org/spargonaut/maxConnectFour/players/InputParserTest.java
@@ -0,0 +1,64 @@
+package org.spargonaut.maxConnectFour.players;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class InputParserTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUp() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void shouldTellUserThatAPlayIsNotValidWhenTheUserDoesNotPlayADigit() throws IOException {
+        BufferedReader bufferedReader = mock(BufferedReader.class);
+        when(bufferedReader.readLine()).thenReturn("C").thenReturn("1");
+
+        InputParser inputParser = new InputParser(bufferedReader);
+        inputParser.getNextPlayFromHuman();
+
+        assertEquals(outContent.toString(), "\nChoose a Column ( 1 - 7 ) -->:\nThat was not a valid digit.\n--Try again\n\nChoose a Column ( 1 - 7 ) -->:");
+    }
+
+    @Test
+    public void shouldAskUserToInputAPlayWhenTheInputIsBlank() throws IOException {
+        BufferedReader bufferedReader = mock(BufferedReader.class);
+        when(bufferedReader.readLine()).thenReturn("").thenReturn("1");
+
+        InputParser inputParser = new InputParser(bufferedReader);
+        inputParser.getNextPlayFromHuman();
+
+        assertEquals(outContent.toString(), "\nChoose a Column ( 1 - 7 ) -->:\nI didn't understand that input\n--Try again\n\nChoose a Column ( 1 - 7 ) -->:");
+    }
+
+    @Test
+    public void shouldParseHumanInputToAnInteger() throws IOException {
+        BufferedReader bufferedReader = mock(BufferedReader.class);
+        when(bufferedReader.readLine()).thenReturn("1");
+
+        InputParser inputParser = new InputParser(bufferedReader);
+        int columnToPlayIn = inputParser.getNextPlayFromHuman();
+
+        assertThat(columnToPlayIn, is(1));
+    }
+}
