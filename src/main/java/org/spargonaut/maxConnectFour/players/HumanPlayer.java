eb8c453422e86f9a5e6cb61cc70b629ec20519c8
jamesspargo <jspargo@thoughtworks.com>
2015-05-09 17:37:04 -0500

    JS - refactor the HumanPlayer class to use the InputParser

--- src/main/java/org/spargonaut/maxConnectFour/players/HumanPlayer.java
+++ src/main/java/org/spargonaut/maxConnectFour/players/HumanPlayer.java
@@ -2,60 +2,33 @@ package org.spargonaut.maxConnectFour.players;
 
 import org.spargonaut.maxConnectFour.gameboard.GameBoard;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 public class HumanPlayer implements Player {
 
-    BufferedReader br = null;
+    private InputParser inputParser;
 
     public HumanPlayer() {
-        this.br = new BufferedReader( new InputStreamReader( System.in ) );
+        inputParser = new InputParser();
     }
 
-    public HumanPlayer(BufferedReader bufferedReader) {
-        this.br = bufferedReader;
+    public HumanPlayer(InputParser inputParser) {
+        this.inputParser = inputParser;
     }
 
     @Override
     public Integer getBestPlay(GameBoard currentGame, int depthLevel) {
-        String userInput = null;
-        Character userChar = 'x';
         int playColumn = -1;
         boolean validPlayFound = false;
+
         do {
+            playColumn = inputParser.getNextPlayFromHuman();
+            playColumn--;
 
-            System.out.print("\nChoose a Column ( 1 - 7 ) -->:");
-            try {
-                userInput = br.readLine();
-            } catch (Exception e) {
-            }
-
-            while( ( userInput.equals("") ) ) {
-                System.out.println("\nI didn't understand that input\n--Try again" );
-                System.out.print("\nChoose a Column ( 1 - 7 ) -->:");
-                try {
-                    userInput = br.readLine();
-                } catch (Exception e) {
-                }
-            }
-
-            userChar = userInput.charAt( 0 );
-
-            if( Character.isDigit( userChar )) {
-                playColumn = Character.getNumericValue( userChar ) - 1;
-
-                if( currentGame.isValidPlay( playColumn ) ) {
-                    validPlayFound = true;
-                } else {
-                    System.out.println("That was an invalid play\n--Try again!");
-                }
-
-
+            if( currentGame.isValidPlay( playColumn ) ) {
+                validPlayFound = true;
             } else {
-                System.out.println("\nThat was not a valid digit.\n--Try again");
-            } // end digit check
-        } while ( !validPlayFound );
+                System.out.println("That was an invalid play\n--Try again!");
+            }
+        } while(!validPlayFound);
 
         return playColumn;
     }
--- src/test/java/org/spargonaut/maxConnectFour/players/HumanPlayerTest.java
+++ src/test/java/org/spargonaut/maxConnectFour/players/HumanPlayerTest.java
@@ -5,7 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.spargonaut.maxConnectFour.gameboard.GameBoard;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -32,58 +31,30 @@ public class HumanPlayerTest {
 
     @Test
     public void shouldReturnAValidColumnToPlayIn() throws IOException {
-        BufferedReader bufferedReader = mock(BufferedReader.class);
-        when(bufferedReader.readLine()).thenReturn("1");
+        InputParser inputParser = mock(InputParser.class);
+        when(inputParser.getNextPlayFromHuman()).thenReturn(1);
 
         GameBoard gameBoard = mock(GameBoard.class);
         when(gameBoard.isValidPlay(0)).thenReturn(true);
 
-        HumanPlayer humanPlayer = new HumanPlayer(bufferedReader);
+        HumanPlayer humanPlayer = new HumanPlayer(inputParser);
         Integer actualValidPlay = humanPlayer.getBestPlay(gameBoard, 2);
 
-        Integer expectedValidPlay = new Integer(0);
-        assertThat(actualValidPlay.intValue(), equalTo(expectedValidPlay.intValue()));
+        int expectedValidPlay = 0;
+        assertThat(actualValidPlay.intValue(), equalTo(expectedValidPlay));
     }
 
     @Test
     public void shouldTellUserThatAPlayIsInvalid() throws IOException {
-        BufferedReader bufferedReader = mock(BufferedReader.class);
-        when(bufferedReader.readLine()).thenReturn("0");
+        InputParser inputParser = mock(InputParser.class);
+        when(inputParser.getNextPlayFromHuman()).thenReturn(0);
 
         GameBoard gameBoard = mock(GameBoard.class);
         when(gameBoard.isValidPlay(-1)).thenReturn(false).thenReturn(true);
 
-        HumanPlayer humanPlayer = new HumanPlayer(bufferedReader);
+        HumanPlayer humanPlayer = new HumanPlayer(inputParser);
         humanPlayer.getBestPlay(gameBoard, 0);
 
-        assertEquals(outContent.toString(), "\nChoose a Column ( 1 - 7 ) -->:That was an invalid play\n--Try again!\n\nChoose a Column ( 1 - 7 ) -->:");
-    }
-
-    @Test
-    public void shouldTellUserThatAPlayIsNotValidWhenTheUserDoesNotPlayADigit() throws IOException {
-        BufferedReader bufferedReader = mock(BufferedReader.class);
-        when(bufferedReader.readLine()).thenReturn("C").thenReturn("1");
-
-        GameBoard gameBoard = mock(GameBoard.class);
-        when(gameBoard.isValidPlay(0)).thenReturn(true);
-
-        HumanPlayer humanPlayer = new HumanPlayer(bufferedReader);
-        humanPlayer.getBestPlay(gameBoard, 0);
-
-        assertEquals(outContent.toString(), "\nChoose a Column ( 1 - 7 ) -->:\nThat was not a valid digit.\n--Try again\n\nChoose a Column ( 1 - 7 ) -->:");
-    }
-
-    @Test
-    public void shouldAskUserToInputAPlayWhenTheInputIsBlank() throws IOException {
-        BufferedReader bufferedReader = mock(BufferedReader.class);
-        when(bufferedReader.readLine()).thenReturn("").thenReturn("1");
-
-        GameBoard gameBoard = mock(GameBoard.class);
-        when(gameBoard.isValidPlay(0)).thenReturn(true);
-
-        HumanPlayer humanPlayer = new HumanPlayer(bufferedReader);
-        humanPlayer.getBestPlay(gameBoard, 0);
-
-        assertEquals(outContent.toString(), "\nChoose a Column ( 1 - 7 ) -->:\nI didn't understand that input\n--Try again\n\nChoose a Column ( 1 - 7 ) -->:");
+        assertEquals(outContent.toString(), "That was an invalid play\n--Try again!\n");
     }
 }
