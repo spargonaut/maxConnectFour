ad71e09395413370b9947e5d5431d9e34ca54852
jamesspargo <jspargo@thoughtworks.com>
2015-05-09 11:52:45 -0500

    JS - refactor a tests to indicate when player one makes a play; add test to indicate when player two makes a play; add test for counting the pieces that have already been played; add tests for indicating which players turn it is;

--- src/test/java/org/spargonaut/maxConnectFour/gameboard/GameBoardTest.java
+++ src/test/java/org/spargonaut/maxConnectFour/gameboard/GameBoardTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.core.IsCollectionContaining.hasItems;
 import static org.junit.Assert.*;
 
@@ -39,7 +40,7 @@ public class GameBoardTest {
     }
 
     @Test
-    public void shouldPlayPieceInTheColumnIndicated() {
+    public void shouldPlayPieceForPlayerOneInTheColumnIndicated() {
         int[][] masterGame = {
                 {0, 0, 0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0, 0, 0},
@@ -66,6 +67,33 @@ public class GameBoardTest {
     }
 
     @Test
+    public void shouldPlayPieceForPlayerTwoInTheColumnIndicated() {
+        int[][] masterGame = {
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0, 0},
+        };
+        GameBoard gameboard = new GameBoard(masterGame);
+
+        gameboard.playPieceInColumn(1);
+
+        int [][] expectedMasterGame = {
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {1, 2, 0, 0, 0, 0, 0},
+        };
+        int[][] actualMasterGame = gameboard.getGameBoard();
+
+        assertArrayEquals(expectedMasterGame, actualMasterGame);
+    }
+
+    @Test
     public void shouldIndicateTheValidColumnsOfPlay() {
         int[][] masterGame = {
                 {1, 0, 1, 0, 0, 2, 0},
@@ -79,5 +107,71 @@ public class GameBoardTest {
 
         List<Integer> validPlays = gameboard.getColumnsOfValidPlays();
         assertThat(validPlays, hasItems(1, 3, 4, 6));
+    }
+
+    @Test
+    public void shouldGetTheCountOfThePiecesAlreadyPlayed() {
+        int[][] masterGame = {
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 2},
+        };
+        GameBoard gameboard = new GameBoard(masterGame);
+
+        int countOfPiecesPlayed = gameboard.getCountOfPiecesPlayed();
+        assertThat(countOfPiecesPlayed, is(2));
+    }
+
+    @Test
+    public void shouldIndicatePlayerOnesTurnWhenPlayCountIsEven() {
+        int[][] masterGame = {
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 2},
+        };
+        GameBoard gameboard = new GameBoard(masterGame);
+
+        int currentPlayersTurn = gameboard.getCurrentTurnBasedOnNumberOfPlays();
+        assertThat(currentPlayersTurn, is(1));
+    }
+
+    @Test
+    public void shouldIndicatePlayerTwosTurnWhenPlayCountIsOdd() {
+        int[][] masterGame = {
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 0},
+        };
+        GameBoard gameboard = new GameBoard(masterGame);
+
+        int currentPlayersTurn = gameboard.getCurrentTurnBasedOnNumberOfPlays();
+        assertThat(currentPlayersTurn, is(2));
+    }
+
+    @Test
+    public void shouldGetNumberOfPlaysRemaining() {
+        int[][] masterGame = {
+                {1, 0, 1, 0, 0, 2, 0},
+                {1, 2, 1, 2, 1, 2, 1},
+                {1, 2, 1, 2, 1, 2, 1},
+                {1, 2, 1, 2, 1, 2, 1},
+                {1, 2, 1, 2, 1, 2, 1},
+                {1, 2, 1, 2, 1, 2, 1},
+        };
+        GameBoard gameBoard = new GameBoard(masterGame);
+
+        int actualPlaysRemaining = gameBoard.getNumberOfPlaysRemaining();
+
+        int expectedPlaysRemaining = 4;
+        assertThat(actualPlaysRemaining, is(expectedPlaysRemaining));
     }
 }
