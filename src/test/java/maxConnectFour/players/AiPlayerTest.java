59ead1bc4eb8de451872e70117860faae195d8ed
jamesspargo <jspargo@thoughtworks.com>
2015-05-08 21:49:30 -0500

    JS - gradleized the project.  builds successfully.  no tests. not run yet.

---
+++ src/test/java/maxConnectFour/players/AiPlayerTest.java
@@ -0,0 +1,40 @@
+package maxConnectFour.players;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AiPlayerTest {
+//
+//	@Test
+//	public void shouldReturnAlphaWhenMaximizingAlphaWithAlphaIsGreaterThanTheScoreOfTheBestMove() {
+//		
+//		AiPlayer aiPlayer = new AiPlayer();
+//		int[] bestMove = {-1, 1};
+//		int expectedAlpha = 999;
+//		int actualAlpha = aiPlayer.maximizeAlphaFOrBestMove(expectedAlpha, bestMove);
+//		assertEquals(expectedAlpha, actualAlpha);
+//	}
+//	
+//	@Test
+//	public void shouldReturnTheScoreOfTheBestMoveWhenMaximizingAlphaWithAlphaIsLessThanTheScoreOfTheBestMove() {
+//		AiPlayer aiPlayer = new AiPlayer();
+//		int[] bestMove = {-1, 999};
+//		int smallAlpha = 1;
+//		
+//		int expectedAlpha = 999;
+//		
+//		int actualAlpha = aiPlayer.maximizeAlphaFOrBestMove(smallAlpha, bestMove);
+//		
+//		assertEquals(expectedAlpha, actualAlpha);
+//	}
+//	
+//	@Test
+//	public void shouldReturnAlphaWhenMaximizingAlphaWithAlphaEqualToTheScoreOfTheBestMove() {
+//		AiPlayer aiPlayer = new AiPlayer();
+//		int expectedAlpha = 999;
+//		int[] bestMove = {-1, expectedAlpha};
+//		int actualAlpha = aiPlayer.maximizeAlphaFOrBestMove(expectedAlpha, bestMove);
+//		assertEquals(expectedAlpha, actualAlpha);
+//	}
+}
