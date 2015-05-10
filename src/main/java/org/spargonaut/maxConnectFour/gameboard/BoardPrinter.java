1253da60eaae4c225dae80527a58f71f7441cb17
jamesspargo <jspargo@thoughtworks.com>
2015-05-10 02:54:56 -0500

    JS - refactor printing the gameboard to a file to use Lists instead of a static array

--- src/main/java/org/spargonaut/maxConnectFour/gameboard/BoardPrinter.java
+++ src/main/java/org/spargonaut/maxConnectFour/gameboard/BoardPrinter.java
@@ -33,11 +33,6 @@ public class BoardPrinter {
         System.out.println(" -----------------\n   1 2 3 4 5 6 7   <---Column numbers");
     }
 
-    /**
-     * this method prints the GameBoard to an output file to be used for
-     * inspection or by another running of the application
-     * @param outputFile the path and file name of the file to be written
-     */
     public void printGameBoardToFile(String outputFile, GameBoard gameboard)
         throws IOException {
 
@@ -45,11 +40,11 @@ public class BoardPrinter {
             outputWriter = new BufferedWriter( new FileWriter( outputFile ) );
         }
 
-        int[][] playBoard = gameboard.getGameBoard();
+        List<List<Integer>> playBoard = gameboard.getGameBoardAsList();
 
-        for( int i = 0; i < 6; i++ ) {
-            for( int j = 0; j < 7; j++ ) {
-                outputWriter.write(playBoard[i][j] + 48);
+        for(List<Integer> row : playBoard) {
+            for(Integer playPiece : row) {
+                outputWriter.write(playPiece + 48);
             }
             outputWriter.write("\n");
         }
