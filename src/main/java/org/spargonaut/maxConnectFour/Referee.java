730b19d2a89afea4b32d98187aca3bbc677677f6
jamesspargo <jspargo@thoughtworks.com>
2015-05-14 00:57:14 -0500

    JS - make the referee save the game to a file

--- src/main/java/org/spargonaut/maxConnectFour/Referee.java
+++ src/main/java/org/spargonaut/maxConnectFour/Referee.java
@@ -1,17 +1,31 @@
 package org.spargonaut.maxConnectFour;
 
 import org.spargonaut.maxConnectFour.gameboard.BoardPrinter;
+import org.spargonaut.maxConnectFour.gameboard.BoardWriter;
 import org.spargonaut.maxConnectFour.gameboard.GameBoard;
 import org.spargonaut.maxConnectFour.gameboard.ScoreKeeper;
 
+import java.io.IOException;
+
 public class Referee {
 
-    private final GameBoard gameboard;
+    private GameBoard gameboard;
+    private BoardWriter boardWriter;
     private PlayMode playMode;
+    private BoardPrinter boardPrinter;
+    private ScoreKeeper scoreKeeper;
 
     public Referee(GameBoard gameboard, PlayMode playmode) {
         this.gameboard = gameboard;
         this.playMode = playmode;
+
+        boardPrinter = new BoardPrinter();
+        scoreKeeper = new ScoreKeeper(gameboard.getGameBoardAsList());
+    }
+
+    public Referee(GameBoard gameBoard, PlayMode oneMove, BoardWriter boardWriter) {
+        this(gameBoard, oneMove);
+        this.boardWriter = boardWriter;
     }
 
     protected void printInitialGameState() {
@@ -19,7 +33,6 @@ public class Referee {
                 "\n\n" +
                 "Max Connect Four Client - " + playMode + " Mode\n");
 
-        BoardPrinter boardPrinter = new BoardPrinter();
         boardPrinter.printGameBoard(gameboard);
 
         ScoreKeeper scoreKeeper = new ScoreKeeper(gameboard.getGameBoardAsList());
@@ -27,9 +40,11 @@ public class Referee {
     }
 
     public void printGameBoardAndScores() {
-        BoardPrinter boardPrinter = new BoardPrinter();
         boardPrinter.printGameBoard(gameboard);
-        ScoreKeeper scoreKeeper = new ScoreKeeper(gameboard.getGameBoardAsList());
         scoreKeeper.printCurrentScores();
+    }
+
+    public void saveGameState(String fileName) throws IOException {
+        boardWriter.printGameBoardToFile(fileName, gameboard);
     }
 }
--- src/test/java/org/spargonaut/maxConnectFour/RefereeTest.java
+++ src/test/java/org/spargonaut/maxConnectFour/RefereeTest.java
@@ -3,9 +3,11 @@ package org.spargonaut.maxConnectFour;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.spargonaut.maxConnectFour.gameboard.BoardWriter;
 import org.spargonaut.maxConnectFour.gameboard.GameBoard;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,6 +15,8 @@ import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class RefereeTest {
 
@@ -30,35 +34,14 @@ public class RefereeTest {
 
     @Test
     public void shouldPrintTheInitialGameStateToTheScreen() {
-        List<List<Integer>> playboard = new ArrayList<List<Integer>>();
-        playboard.add(Arrays.asList(1, 1, 1, 1, 0, 0, 0));
-        playboard.add(createBlankRow());
-        playboard.add(createBlankRow());
-        playboard.add(createBlankRow());
-        playboard.add(createBlankRow());
-        playboard.add(createBlankRow());
-        GameBoard gameboard = new GameBoard(playboard);
-
+        GameBoard gameboard = createGameBoard();
         PlayMode playmode = PlayMode.ONE_MOVE;
         Referee referee = new Referee(gameboard, playmode);
 
         String expectedInitialGameBoardMessage = "--------------------------------------------------------------------------------" +
                 "\n\n" +
                 "Max Connect Four Client - " + playmode + " Mode" +
-                "\n\n" +
-                "   1 2 3 4 5 6 7   <---  Column numbers\n" +
-                " -----------------\n" +
-                "_| 1 1 1 1       |_\n" +
-                "_|               |_\n" +
-                "_|               |_\n" +
-                "_|               |_\n" +
-                "_|               |_\n" +
-                "_|               |_\n" +
-                " -----------------\n" +
-                "   1 2 3 4 5 6 7   <---Column numbers\n" +
-                "Scores:\n" +
-                " Player1: 1\n" +
-                " Player2: 0\n\n";
+                "\n\n" + createBoardAndScoresString();
         referee.printInitialGameState();
 
         assertThat(outContent.toString(), is(expectedInitialGameBoardMessage));
@@ -66,6 +49,39 @@ public class RefereeTest {
 
     @Test
     public void shouldPrintTheGameBoardStateToTheScreen() {
+        GameBoard gameboard = createGameBoard();
+        PlayMode playmode = PlayMode.ONE_MOVE;
+        Referee referee = new Referee(gameboard, playmode);
+
+        referee.printGameBoardAndScores();
+
+        String expectedMessage = createBoardAndScoresString();
+
+        assertThat(outContent.toString(), is(expectedMessage));
+    }
+
+    @Test
+    public void shouldSaveTheGameBoardToAFile() throws IOException {
+        List<List<Integer>> setUpGameBoard = new ArrayList<List<Integer>>();
+        setUpGameBoard.add(Arrays.asList(1, 1, 1, 1, 0, 0, 0));
+        setUpGameBoard.add(createBlankRow());
+        setUpGameBoard.add(createBlankRow());
+        setUpGameBoard.add(createBlankRow());
+        setUpGameBoard.add(createBlankRow());
+        setUpGameBoard.add(createBlankRow());
+
+        GameBoard gameBoard = new GameBoard(setUpGameBoard);
+
+        BoardWriter boardWriter = mock(BoardWriter.class);
+
+        Referee referee = new Referee(gameBoard, PlayMode.ONE_MOVE, boardWriter);
+        String setupFileName = "some/file/name.txt";
+        referee.saveGameState(setupFileName);
+
+        verify(boardWriter).printGameBoardToFile(setupFileName, gameBoard);
+    }
+
+    private GameBoard createGameBoard() {
         List<List<Integer>> playboard = new ArrayList<List<Integer>>();
         playboard.add(Arrays.asList(1, 1, 1, 1, 0, 0, 0));
         playboard.add(createBlankRow());
@@ -73,14 +89,11 @@ public class RefereeTest {
         playboard.add(createBlankRow());
         playboard.add(createBlankRow());
         playboard.add(createBlankRow());
-        GameBoard gameboard = new GameBoard(playboard);
+        return new GameBoard(playboard);
+    }
 
-        PlayMode playmode = PlayMode.ONE_MOVE;
-        Referee referee = new Referee(gameboard, playmode);
-
-        referee.printGameBoardAndScores();
-
-        String expectedMessage = "   1 2 3 4 5 6 7   <---  Column numbers\n" +
+    private String createBoardAndScoresString() {
+        return "   1 2 3 4 5 6 7   <---  Column numbers\n" +
                 " -----------------\n" +
                 "_| 1 1 1 1       |_\n" +
                 "_|               |_\n" +
@@ -93,8 +106,6 @@ public class RefereeTest {
                 "Scores:\n" +
                 " Player1: 1\n" +
                 " Player2: 0\n\n";
-
-        assertThat(outContent.toString(), is(expectedMessage));
     }
 
     private List<Integer> createBlankRow() {
