c8fbba89e431136e939c18cd2bc6fd99ad4f25ce
jamesspargo <jspargo@thoughtworks.com>
2015-05-16 01:28:41 -0500

    JS - removed dead code from MaxConnectFour

--- src/main/java/org/spargonaut/maxConnectFour/MaxConnectFour.java
+++ src/main/java/org/spargonaut/maxConnectFour/MaxConnectFour.java
@@ -1,9 +1,7 @@
 package org.spargonaut.maxConnectFour;
 
-import org.spargonaut.maxConnectFour.gameboard.BoardPrinter;
 import org.spargonaut.maxConnectFour.gameboard.BoardReader;
 import org.spargonaut.maxConnectFour.gameboard.GameBoard;
-import org.spargonaut.maxConnectFour.gameboard.ScoreKeeper;
 import org.spargonaut.maxConnectFour.parser.ArgumentParser;
 import org.spargonaut.maxConnectFour.players.AiPlayer;
 import org.spargonaut.maxConnectFour.players.HumanPlayer;
@@ -16,7 +14,6 @@ public class MaxConnectFour {
 
     private PlayMode playMode;
     private String input;
-    private int depthLevel;
 
     private ArgumentParser argumentParser;
 
@@ -40,9 +37,6 @@ public class MaxConnectFour {
         AiPlayer calculon = new AiPlayer(argumentParser.getSearchDepth());
         HumanPlayer human = new HumanPlayer();
 
-        //  variables to keep up with the game
-        int playColumn = 99;				//  the players choice of column to play
-
         switch(playMode) {
         case INTERACTIVE:
             InteractiveReferee referee = new InteractiveReferee(currentGame, human, calculon, argumentParser.getNextPlayer());
@@ -58,34 +52,10 @@ public class MaxConnectFour {
         }
     }
 
-    protected void printInitialGameBoardState(GameBoard currentGame) {
-        System.out.println( "--------------------------------------------------------------------------------");
-        System.out.println("\nMax Connect Four Client - " + playMode + " Mode\n");
-        printBoardAndScores(currentGame);
-    }
-
-    protected void printBoardAndScores(GameBoard currentGame) {
-        BoardPrinter boardPrinter = new BoardPrinter();
-        boardPrinter.printGameBoard(currentGame);
-        ScoreKeeper scoreKeeper = new ScoreKeeper(currentGame.getGameBoardAsList());
-        scoreKeeper.printCurrentScores();
-    }
-
-    protected void printCurrentGameBoardAndScores(GameBoard currentGame) {
-        System.out.println("\n...and now the board looks like this: \n");
-        printBoardAndScores(currentGame);
-    }
-
-    private void printTheFinalGameState(GameBoard currentGame) {
-        System.out.println("Here is the final game state\n");
-        printBoardAndScores(currentGame);
-    }
-
     private void parseInputArguments(String[] args) {
         argumentParser = new ArgumentParser();
         argumentParser.parseArguments(args);
         playMode = argumentParser.getPlayMode();
         input = argumentParser.getInputGameFile();
-        depthLevel = argumentParser.getSearchDepth();
     }
 }
